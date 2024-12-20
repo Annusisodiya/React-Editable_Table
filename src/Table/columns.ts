10d53b47c7c2dff1d1f2f1a6163f04789491bc8b
Anita sisodiya <126265930+Annusisodiya@users.noreply.github.com>
2024-12-20 15:19:21 +0530

    complete update Editable_Table
    Key features:
    
    Editable fields that update in real-time
    Ability to add new rows
    "Save Data" button that prepares data for API submission
    Clean, responsive design
    No backend required

---
+++ src/Table/columns.ts
@@ -0,0 +1,59 @@
+import { createColumnHelper } from '@tanstack/react-table'
+import { TableCell } from './TableCell'
+import { Student } from './types'
+import { EditCell } from './EditCell'
+
+const columnHelper = createColumnHelper<Student>()
+
+export const columns = [
+  columnHelper.accessor('studentNumber', {
+    header: 'Student ID',
+    cell: TableCell,
+    meta: {
+      type: 'number',
+    },
+  }),
+  columnHelper.accessor('name', {
+    header: 'Full Name',
+    cell: TableCell,
+    meta: {
+      type: 'text',
+      required: true,
+      pattern: '^[a-zA-Z ]+$',
+    },
+  }),
+  columnHelper.accessor('dateOfBirth', {
+    header: 'Date Of Birth',
+    cell: TableCell,
+    meta: {
+      type: 'date',
+      required: true,
+      validate: (value: string) => {
+        const date = new Date(value);
+        const today = new Date();
+        return date <= today;
+      },
+      validationMessage: 'Date cannot be in the future',
+    },
+  }),
+  columnHelper.accessor('major', {
+    header: 'Major',
+    cell: TableCell,
+    meta: {
+      type: 'select',
+      options: [
+        { value: '', label: 'Select' },
+        { value: 'Computer Science', label: 'Computer Science' },
+        { value: 'Communications', label: 'Communications' },
+        { value: 'Business', label: 'Business' },
+        { value: 'Psychology', label: 'Psychology' },
+        { value: 'Full Stack Develper', label: 'Full Stack' },
+      ],
+      required: true,
+    },
+  }),
+  columnHelper.display({
+    id: 'edit',
+    cell: EditCell,
+  }),
+]
